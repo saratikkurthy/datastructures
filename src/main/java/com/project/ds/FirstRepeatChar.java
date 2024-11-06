686af74b296ee388b0bea97663afff03f0b2d3f9
saratikkurthy <saratikkurthy@gmail.com>
2024-11-06 12:35:44 -0800

    First Repeated character with time complexity O(n)

--- src/main/java/com/project/ds/FirstRepeatChar.java
+++ src/main/java/com/project/ds/FirstRepeatChar.java
@@ -1,7 +1,21 @@
 package com.project.ds;
 
+import java.util.HashMap;
+
 public class FirstRepeatChar {
     public static void main(String[] args){
+        String inputStr="responsible";
+        char[] inputArr=inputStr.toCharArray();
+        HashMap<Character,Boolean> charMap=new HashMap<Character, Boolean>();
+        for(char c:inputArr){
+            if(charMap.get(c)==null){
+                charMap.put(c,true);
+            }
+            else{
+                System.out.println("Found first repeating sequence for character:"+c);
+                break;
+            }
 
+        }
     }
 }

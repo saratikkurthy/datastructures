38fb37b36fa762acfeab7e69d1316b87a107ad4f
saratikkurthy <saratikkurthy@gmail.com>
2024-11-06 11:53:00 -0800

    Use case 1

--- src/main/java/com/project/ds/SumToK.java
+++ src/main/java/com/project/ds/SumToK.java
@@ -4,10 +4,21 @@ import java.util.HashMap;
 
 public class SumToK {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args){
-        int[] nums = {1,2,3,4,5,6,7,8};
-        System.out.println(findPair(nums,6));
+        int[] nums = {3,11,5,2,1,6};
+        System.out.println(findPair(nums,7));
     }
+
+    /**
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
     static boolean findPair(int[] arr, int k){
         HashMap<Integer,Boolean> visited = new HashMap<Integer,Boolean>();
         for(int element : arr){

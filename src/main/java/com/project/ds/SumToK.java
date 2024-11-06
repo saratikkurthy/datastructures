55e72e7d2f244b62c9d5e561862ca42b05e8d700
saratikkurthy <saratikkurthy@gmail.com>
2024-11-06 11:18:14 -0800

    Initial project

--- src/main/java/com/project/ds/SumToK.java
+++ src/main/java/com/project/ds/SumToK.java
@@ -8,7 +8,6 @@ public class SumToK {
         int[] nums = {1,2,3,4,5,6,7,8};
         System.out.println(findPair(nums,6));
     }
-
     static boolean findPair(int[] arr, int k){
         HashMap<Integer,Boolean> visited = new HashMap<Integer,Boolean>();
         for(int element : arr){

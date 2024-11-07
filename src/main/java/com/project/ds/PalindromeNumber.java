a32839fc57145242154e0b7657e7462be5f9418a
saratikkurthy <saratikkurthy@gmail.com>
2024-11-07 12:33:17 -0800

    Palindrome number

---
+++ src/main/java/com/project/ds/PalindromeNumber.java
@@ -0,0 +1,16 @@
+package com.project.ds;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int number){
+        int x=number;
+        int reverse=0;
+        if(number<0 || (number!=0 && number%10==0))
+            return false;
+        while (number!=0){
+            int lastDigit=number%10;
+            reverse=reverse*10+lastDigit;
+            number=number/10;
+        }
+        return (x==reverse/10 || x==reverse);
+    }
+}
---
+++ src/test/java/com/project/ds/test/PalindromeNumberTest.java
@@ -0,0 +1,11 @@
+package com.project.ds.test;
+
+import com.project.ds.PalindromeNumber;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PalindromeNumberTest {
+    @Test
+    public void testIsPalindrome(){
+        assertEquals(true, PalindromeNumber.isPalindrome(34543));
+    }
+}

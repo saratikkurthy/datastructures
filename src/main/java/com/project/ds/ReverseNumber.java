50eeb70ceaf19b5892bf9bc02663b806784b7c47
saratikkurthy <saratikkurthy@gmail.com>
2024-11-07 12:07:08 -0800

    Identify single non duplicate using XOR

---
+++ src/main/java/com/project/ds/ReverseNumber.java
@@ -0,0 +1,20 @@
+package com.project.ds;
+
+public class ReverseNumber {
+    public static int reverse(int num) {
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        int result = 0;
+        while (num != 0) {
+            int lastDigit = num % 10;
+            if (result > max / 10 || (result == max / 10 && lastDigit > 7))
+                return 0;
+            if (result < min / 10 || (result == min / 10 && lastDigit < -8))
+                return 0;
+            result = result * 10 + lastDigit;
+            num = num / 10;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/project/ds/test/ReverseNumberTest.java
@@ -0,0 +1,13 @@
+package com.project.ds.test;
+
+import com.project.ds.ReverseNumber;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ReverseNumberTest {
+    @Test
+    public void testReverse() {
+        assertEquals(5431, ReverseNumber.reverse(1345));
+        assertEquals(321, ReverseNumber.reverse(1230));
+    }
+}

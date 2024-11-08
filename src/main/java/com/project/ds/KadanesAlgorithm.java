182925131f8958857f35932759ac15ff47a0dcbc
saratikkurthy <saratikkurthy@gmail.com>
2024-11-08 11:30:11 -0800

    Kandanes Algorithm DS

---
+++ src/main/java/com/project/ds/KadanesAlgorithm.java
@@ -0,0 +1,35 @@
+package com.project.ds;
+
+public class KadanesAlgorithm {
+    /**
+     * Brute force wth Time complexity O(n)=n^2
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxSubArraySumBF(int[] nums) {
+        int sum = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+                sum = Math.max(sum, currentSum);
+            }
+        }
+        return sum;
+    }
+
+    public static int maxSubArrayKadane(int[] nums) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // Find the maximum sum ending at index i by either extending
+            // the maximum sum sub array ending at index i - 1 or by
+            // starting a new sub array from index i
+            maxSum = Math.max(maxSum + nums[i], nums[i]);
+            // Update sum if maximum sub array sum ending at index i > res
+            sum = Math.max(sum, maxSum);
+        }
+        return sum;
+    }
+}
--- src/test/java/com/project/patterns/creational/SingletonTest.java
+++ src/test/java/com/project/patterns/creational/SingletonTest.java
@@ -1,12 +1,14 @@
 package com.project.patterns.creational;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class SingletonTest {
     @Test
-    public void testGetInstance(){
-       assertNotSame(null,Singleton.getInstance());
-       Object obj=Singleton.getInstance();
-       assertEquals(obj,Singleton.getInstance());
+    public void testGetInstance() {
+        assertNotSame(null, Singleton.getInstance());
+        Object obj = Singleton.getInstance();
+        assertEquals(obj, Singleton.getInstance());
     }
 }

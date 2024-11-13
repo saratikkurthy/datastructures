ea106d972ce79d007ea344cba63f7d2fad9af6d8
saratikkurthy <saratikkurthy@gmail.com>
2024-11-13 13:26:45 -0800

    Longest Prefix with Time complexity O(m*n)

---
+++ src/main/java/com/project/ds/LongestPrefix.java
@@ -0,0 +1,30 @@
+package com.project.ds;
+
+public class LongestPrefix {
+    /**
+     * @param strings
+     * @return
+     */
+    public static String longestPrefix(String[] strings) {
+        if (strings.length == 0) {
+            return "";
+        }
+        String prefix = strings[0];
+        for (int i = 1; i < strings.length; i++) {
+            while (strings[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String[] strings = {"Test", "Testing", "TestResults"};
+        String prefix = longestPrefix(strings);
+        System.out.println(prefix);
+
+    }
+}

17e38a7f0edfce3ec6f17e3d37b1d8a3d6d7258c
saratikkurthy <saratikkurthy@gmail.com>
2024-11-28 07:48:45 -0800

    Factory pattern with List demo

--- src/main/java/com/project/patterns/creational/FactoryClient.java
+++ src/main/java/com/project/patterns/creational/FactoryClient.java
@@ -1,5 +1,7 @@
 package com.project.patterns.creational;
 
+import java.util.*;
+
 public class FactoryClient {
     public static void main(String[] args){
         Shape shape=new CircleFactory().getShape();
@@ -8,5 +10,32 @@ public class FactoryClient {
         shape.draw();
         shape=new SquareFactory().getShape();
         shape.draw();
+
+        List myList;
+        myList=new ArrayList();
+        myList.add(5);
+        myList.add(4);
+        myList.add(3);
+        myList.add(2);
+        System.out.println(myList);
+        myList=new LinkedList();
+        myList.add(5);
+        myList.add(4);
+        myList.add(3);
+        myList.add(2);
+        System.out.println(myList);
+        myList=new Stack();
+        myList.add(5);
+        myList.add(4);
+        myList.add(3);
+        myList.add(2);
+        System.out.println(myList);
+        myList=new Vector();
+        myList.add(5);
+        myList.add(4);
+        myList.add(3);
+        myList.add(2);
+        System.out.println(myList);
+
     }
 }

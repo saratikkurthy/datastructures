e47cc48522644056efde5ba39142e9b8e64e5d9e
saratikkurthy <saratikkurthy@gmail.com>
2024-12-04 12:31:26 -0800

    Binary Tree Basic

---
+++ src/main/java/com/project/ds/trees/BinTree.java
@@ -0,0 +1,28 @@
+package com.project.ds.trees;
+
+class Node {
+    int data;
+    Node left, right;
+    Node(int d) {
+        data = d;
+        left = null;
+        right = null;
+    }
+}
+
+class BinTree {
+    public static void main(String[] args) {
+        // Initialize and allocate memory for tree nodes
+        Node firstNode = new Node(2);
+        Node secondNode = new Node(3);
+        Node thirdNode = new Node(4);
+        Node fourthNode = new Node(5);
+
+        // Connect binary tree nodes
+        firstNode.left = secondNode;
+        firstNode.right = thirdNode;
+        secondNode.left = fourthNode;
+        System.out.println("Node:"+firstNode.left.data);
+
+    }
+}
